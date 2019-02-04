ca5c54323272d47efbd92832ad83cd6107eeb4e1
jmarkicg <jele.markic@gmail.com>
2019-02-04 22:12:54 +0100

    Initial commit.

--- src/main/java/hr/markic/rlc/RlcCombinatorApplication.java
+++ src/main/java/hr/markic/rlc/RlcCombinatorApplication.java
@@ -1,5 +1,8 @@
 package hr.markic.rlc;
 
+import hr.markic.rlc.config.PropertyConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RlcCombinatorApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(PropertyConfig.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RlcCombinatorApplication.class, args);
-		System.out.println("test");
+		log.info("------------------Application started.----------------------");
 	}
 
 }
