cccfa6df5493ac2f1733663a252fa8c5e9989b63
jmarkic <borovnica17@gmail.com>
2019-04-26 00:13:02 +0200

    Implemented JWT security for backend.

---
+++ src/main/java/hr/markic/rlc/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package hr.markic.rlc.repository;
+
+import hr.markic.rlc.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
