655fd8757fa89d55d8decfb2c0fca38b26a8bcf8
jmarkicg <jele.markic@gmail.com>
2020-11-01 15:37:01 +0100

    Added repository annotation.

--- src/main/java/hr/markic/rlc/repository/UserRepository.java
+++ src/main/java/hr/markic/rlc/repository/UserRepository.java
@@ -2,9 +2,11 @@ package hr.markic.rlc.repository;
 
 import hr.markic.rlc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
